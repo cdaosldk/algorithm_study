3982850fe0cf350394679cec28d10e2d6b275959
wannaDevelopIt <cdaosldk@naver.com>
2026-07-05 02:30:16 +0900

    1. 배열 조각하기_문제 설명

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+// 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/181893
+// 배열 조각하기
+// 정수 배열 arr와 query가 주어집니다.
+
+// query를 순회하면서 다음 작업을 반복합니다.
+
+// 짝수 인덱스에서는 arr에서 query[i]번 인덱스를 제외하고 배열의 query[i]번 인덱스 뒷부분을 잘라서 버립니다.
+// 홀수 인덱스에서는 arr에서 query[i]번 인덱스는 제외하고 배열의 query[i]번 인덱스 앞부분을 잘라서 버립니다.
+
 import java.util.Arrays;
 
 class Solution {
